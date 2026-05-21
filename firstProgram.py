8f834b37b25b2f38816d7cd1caba31e2b70cbc8b
Asif <asif@Asifs-MacBook-Air.local>
2026-05-21 12:13:53 +0530

    first-commit

---
+++ firstProgram.py
@@ -0,0 +1,10 @@
+with open("demo.txt", "r+") as f:
+    data = f.read()
+    print(data)
+
+new_data =  data.replace("java", "python")
+print(new_data)
+
+
+with open("demo.txt", "w+") as f:
+    f.write(new_data)
